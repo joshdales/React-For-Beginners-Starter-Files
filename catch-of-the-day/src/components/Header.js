b5170f96a1144ad068af7633f42ed0928ea87777
Josh Dales <dales.josh@gmail.com>
2018-01-08 15:36:12 -0500

    Pass tagline to Header as props and render title in header

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 class Header extends React.Component {
   render () {
+    const {tagline} = this.props
     return (
-    <p>Header</p>
+    <header className="top">
+      <h1>Catch of the Day</h1>
+      <h3 className="tagline">{tagline}</h3>
+    </header>
   )};
 };
 
