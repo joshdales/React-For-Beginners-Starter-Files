5f89734cf5d215c3af1a5010e345e58f0c905670
Josh Dales <dales.josh@gmail.com>
2018-01-08 16:25:51 -0500

    Create function to change the url to submitted value

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  goToStore(e) {
+    e.preventDefault();
+    console.log("You changed the url");
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
         <button type="submit">Visit Store</button>
