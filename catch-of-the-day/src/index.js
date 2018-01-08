5ca1cad842d4ff0689c2533fc3a08441c7329c61
Josh Dales <dales.josh@gmail.com>
2018-01-08 14:53:04 -0500

    Export index.js to index.html

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { render } from 'react-dom';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>'Hello'</p>
+    return <p>Hello</p>
   }
 }
+
+render(<StorePicker/>, document.querySelector('#main'))
