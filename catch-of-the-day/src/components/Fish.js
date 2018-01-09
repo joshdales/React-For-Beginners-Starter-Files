446ea1c144c5b1919b8872da31ad41650b3128a6
Josh Dales <dales.josh@gmail.com>
2018-01-09 15:59:04 -0500

    Save fish props as consts, and display name

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 class Fish extends React.Component {
   render() {
+    const { name, price, status, desc, image } = this.props.fish;
     return (
-      <li className="menu-fish">Fish</li>
+      <li className="menu-fish">
+        {name}
+      </li>
     );
   };
 };
