981f872d3637e2c3dcd1d989db9e16406ff46de0
Josh Dales <dales.josh@gmail.com>
2018-01-09 13:28:19 -0500

    Prevent default form action for AddFishForm

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
+  createFish(event) {
+    event.preventDefault();
+    console.log('Gonna get some fish! 🎣');
+  }
+
   render() {
     return (
-      <form className="fish-edit">
+      <form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
         <input type="text" placeholder="Fish Name"/>
         <input type="text" placeholder="Fish Price"/>
         <select>
