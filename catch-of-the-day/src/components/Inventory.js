33dfbbe5cb984d5ac875f222c18a6f434b96be2a
Josh Dales <dales.josh@gmail.com>
2018-01-09 15:13:27 -0500

    Import smapleFishes into App and add button to Inventory to load

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -7,6 +7,7 @@ class Inventory extends React.Component {
       <section>
         <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish}/>
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </section>
     );
   };
