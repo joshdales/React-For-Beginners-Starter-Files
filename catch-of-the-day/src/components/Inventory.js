cdfc278e6b0efbef5d060ae120471b6b615bec4c
Josh Dales <dales.josh@gmail.com>
2018-01-09 13:16:12 -0500

    Create AddFishform component

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Inventory extends React.Component {
   render() {
     return(
-      <p>Inventory</p>
+      <section>
+        <h2>Inventory</h2>
+      </section>
     );
   };
 };
