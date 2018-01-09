75ad517b139fb48f0bfe25741144394a0ede5fad
Josh Dales <dales.josh@gmail.com>
2018-01-09 16:50:15 -0500

    Format price in Fish

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {formatPrice} from '../helpers';
 
 class Fish extends React.Component {
   render() {
@@ -7,7 +8,7 @@ class Fish extends React.Component {
       <li className="menu-fish">
         <img src={image} alt={name}/>
         <h3 className="fish-name">{name}
-          <span className="price">{price}</span>
+          <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
         <button>Add To Order</button>
