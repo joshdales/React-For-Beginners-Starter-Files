979689a75d3c0e74ff0b41f7c920b9723e14cb0d
Josh Dales <dales.josh@gmail.com>
2018-01-09 13:48:58 -0500

    Store form values in fish and log

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -5,25 +5,26 @@ class AddFishForm extends React.Component {
     event.preventDefault();
     console.log('Gonna get some fish! 🎣');
     const fish = {
-      name
-      price
-      status
-      desc
-      image
+      name: this.name.value,
+      price: this.price.value,
+      status: this.status.value,
+      desc: this.desc.value,
+      image: this.image.value
     }
+    console.log(fish);
   }
 
   render() {
     return (
       <form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
-        <input type="text" placeholder="Fish Name"/>
-        <input type="text" placeholder="Fish Price"/>
-        <select>
+        <input ref={(input) => this.name = input} type="text" placeholder="Fish Name"/>
+        <input ref={(input) => this.price = input} type="text" placeholder="Fish Price"/>
+        <select ref={(input) => this.status = input}>
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea placeholder="Fish Desc."/>
-        <input type="text" placeholder="Fish Image"/>
+        <textarea ref={(input) => this.desc = input} placeholder="Fish Desc."/>
+        <input ref={(input) => this.image = input} type="text" placeholder="Fish Image"/>
         <button type="submit">+ Add Item</button>
       </form>
     )
