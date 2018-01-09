f5081ede3ceb861fbb4579d14db947b21c3ce4aa
Josh Dales <dales.josh@gmail.com>
2018-01-09 16:06:33 -0500

    display all fish info in Fish

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -5,7 +5,12 @@ class Fish extends React.Component {
     const { name, price, status, desc, image } = this.props.fish;
     return (
       <li className="menu-fish">
-        {name}
+        <img src={image} alt={name}/>
+        <h3 className="fish-name">{name}
+          <span className="price">{price}</span>
+        </h3>
+        <p>{desc}</p>
+        <button>Add To Order</button>
       </li>
     );
   };
