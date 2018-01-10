43c2c48ba7768234c396007935cf0b28b66f06ac
Josh Dales <dales.josh@gmail.com>
2018-01-10 10:20:43 -0500

    Create total function in Order

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 class Order extends React.Component {
   render() {
-    const { fishes, order } = this.props
+    const { fishes, order } = this.props;
+    const orderIDs = Object.keys(order);
+    const total = orderIDs.reduce((prevTotal, key) => {
+      const fish = fishes[key];
+      const count = order[key];
+      const isAvailable = fish && fish.status === 'available';
+
+      if (isAvailable) {
+        return prevTotal + (count * fish.price || 0);
+      }
+      return prevTotal
+    }, 0);
     return (
       <div className="order-wrap">
         <h2>Your Order</h2>
