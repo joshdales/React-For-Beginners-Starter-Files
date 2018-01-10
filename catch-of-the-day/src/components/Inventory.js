de92dba4d407d2d1eea906fe09fb2d797a6c5c04
Josh Dales <dales.josh@gmail.com>
2018-01-10 13:04:01 -0500

    Map through fishes and create function to render

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -2,11 +2,18 @@ import React from 'react';
 import AddFishForm from './AddFishForm'
 
 class Inventory extends React.Component {
+
+  renderInventory(key) {
+    return (
+      <div className="fish-edit" key={key}></div>
+    )
+  }
+
   render() {
     return(
       <section>
         <h2>Inventory</h2>
-        {Object.keys(this.props.fishes)}
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
         <AddFishForm addFish={this.props.addFish}/>
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </section>
